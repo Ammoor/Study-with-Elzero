01755cb9ca2ba66937f27dcb601734fa426bf6ed
Omar-Ayman-Gaber <149304486+Omar-Ayman-Gaber@users.noreply.github.com>
2024-06-16 22:38:38 +0300

    Create Assignment 4.js

---
+++ Assignments/JS/001 : 009/Assignment 4.js
@@ -0,0 +1,25 @@
+// Assignment 4:
+
+console.group("Group 1");
+console.log("Message One");
+console.log("Message Two");
+console.group("Child Group");
+console.log("Message One");
+console.log("Message Two");
+console.group("Grand Child Group");
+console.log("Message One");
+console.log("Message Two");
+console.groupEnd(); // Ends "Grand Child Group" group.
+console.groupEnd(); // Ends "Grand Child Group" group.
+console.groupEnd(); // Ends "Group 1" group.
+console.group("Group 2");
+console.log("Message One");
+console.log("Message Two");
+
+/*
+
+1- console.group(): Create a group of messages in the console.
+2- console.groupEnd(): Ends a message group in the console.
+3- console.groupCollapsed(): Create a collapsed group of messages in the console.
+
+*/

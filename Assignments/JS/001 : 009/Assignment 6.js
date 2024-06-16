218e6ecf19d88409a82d1e3723b4e07c5746d54a
Omar-Ayman-Gaber <149304486+Omar-Ayman-Gaber@users.noreply.github.com>
2024-06-16 21:27:24 +0300

    Create Assignment 6.js

---
+++ Assignments/JS/001 : 009/Assignment 6.js
@@ -0,0 +1,25 @@
+// Assignment 6:
+
+/*
+
+To stop the execution of the code without deleting the code or modifying it we can simply
+put the code into comment.
+
+And we can do this in two ways:
+1- Single line comment.
+2- Multiple line comment.
+
+*/
+
+// The first method: (Single line comment)
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+
+/*
+
+The second method: (Multiple line comment)
+
+console.log("Iam In Console");
+document.write("Iam In Page");
+
+*/

656e2f72dd420599a9cdff7f3c5c216d83fb5118
Omar-Ayman-Gaber <149304486+Omar-Ayman-Gaber@users.noreply.github.com>
2024-06-16 22:07:29 +0300

    Update Assignment 3.js

--- Assignments/JS/001 : 009/Assignment 3.js
+++ Assignments/JS/001 : 009/Assignment 3.js
@@ -2,7 +2,7 @@
 
 console.log(
   "%cElzero %cWeb %cSchool",
-  "color:red;font-size:40px;",
-  "color:green;font-size:40px;font-weight:bold;",
-  "background-color:blue;font-size:40px;"
+  "color: red; font-size: 40px;",
+  "color: green; font-size: 40px; font-weight: bold;",
+  "background-color: blue; font-size: 40px;"
 );

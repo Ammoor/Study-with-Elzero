36b29e9c9840259ea9bdb443c47b63885304c4ed
Omar-Ayman-Gaber <149304486+Omar-Ayman-Gaber@users.noreply.github.com>
2024-06-16 20:34:11 +0300

    Create Assignment 1.js

---
+++ Assignments/JS/001 : 009/Assignment 1.js
@@ -0,0 +1,14 @@
+/*
+
+Assignment 1:
+
+The code that will not execute is the code number one.
+
+And the reason is because the code number one tries to access an element that is not created yet in the
+Document Object Model (DOM).
+
+Unlike code number two that will wait the entire page to load and then will access the element and the code
+number three which in the end of the body tag that will access the element after all the content created in the 
+DOM.
+
+*/

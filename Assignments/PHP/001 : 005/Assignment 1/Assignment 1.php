86b83a39d53305683b96bb1706097d2a40d1942b
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-08-03 19:15:06 +0300

    Create Assignment 1.php

---
+++ Assignments/PHP/001 : 005/Assignment 1/Assignment 1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php echo "UTF-8"; ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?=
+        "My First PHP Page"
+        ?>
+    </title>
+</head>
+<body>
+    <?php
+    /* 
+        Assignment 1 :
+    */
+    ?>
+    <div>
+        <?php
+        echo "We Love";
+        ?>
+    </div>
+    <div>
+        <?php
+        echo "Elzero Channel";
+        ?>
+    </div>
+</body>
+</html>

3f40eabf6d3b9bea1bdb3adb1a283625dc939216
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-08-03 19:35:16 +0300

    Create Assignment 4.php

---
+++ Assignments/PHP/001 : 005/Assignment 4/Assignment 4.php
@@ -0,0 +1,11 @@
+<?php
+// Assignment 4 :
+
+echo "YES<br/>";
+// ## First Comment
+echo "NO<br/>";
+# // # Second Comment
+echo "YES<br/>";
+/* /* /* Third Comment */
+echo "NO<br/>";
+////// Fourth Comment  

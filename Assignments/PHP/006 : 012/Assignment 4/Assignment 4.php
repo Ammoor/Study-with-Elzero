503b37a57ce8fbee8fb44f77df0251f367479d31
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-08-21 10:06:29 +0300

    Update Assignment 4.php

--- Assignments/PHP/006 : 012/Assignment 4/Assignment 4.php
+++ Assignments/PHP/006 : 012/Assignment 4/Assignment 4.php
@@ -1,6 +1,11 @@
 <?php
 // Assignment 4:
+
+// Edited code:
 echo nl2br("We \n Love \n Elzero \n Web \n School");
+
+// Original code:
+// echo "We \n Love \n Elzero \n Web \n School";
 
 /* 
 Needed Output:
@@ -14,6 +19,6 @@ School
 
 /*
 Important Note:
-    This code will be in multiple lines in the terminal not in the browser.
-    To make it in multiple lines we will use "nl2br" function.
+    The original code will be in multiple lines in the terminal not in the browser.
+    To make it in multiple lines we will use the "nl2br" function.
 */

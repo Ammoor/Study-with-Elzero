432f6d9d88994cfae1c9df5650f7c903a7951c26
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-08-21 16:43:52 +0300

    Create Assignment 8.php

---
+++ Assignments/PHP/006 : 012/Assignment 8/Assignment 8.php
@@ -0,0 +1,38 @@
+<?php
+// Assignment 8:
+
+echo "<pre>";
+print_r(
+    [
+        "FrontEnd" =>
+        [
+            "HTML",
+            "CSS",
+            "JS" =>
+            [
+                "Vuejs" =>
+                [
+                    2 => "v2",
+                    3 => "v3"
+                ],
+                "Reactjs",
+                "Svelte"
+            ]
+        ],
+        "BackEnd" =>
+        [
+            "PHP",
+            "MySQL",
+            "Security"
+        ],
+        "Git",
+        "GitHub",
+        "Testing" =>
+        [
+            "Unit Testing",
+            "End To End",
+            "Integration"
+        ]
+    ]
+);
+echo "</pre>";

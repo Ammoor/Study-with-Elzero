dd006fe113f2ac707c580a23a51344683666f893
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-08-21 13:49:25 +0300

    Create Assignment 5.php

---
+++ Assignments/PHP/006 : 012/Assignment 5/Assignment 5.php
@@ -0,0 +1,17 @@
+<?php
+// Assignment 5:
+
+$var = <<<'Nowdoc'
+Hello "'Elzero'"
+We Love $Programming$
+Languages Specially "PHP"
+Nowdoc;
+echo nl2br($var);
+
+/* 
+Needed Output:
+
+Hello "'Elzero'"
+We Love $Programming$
+Languages Specially "PHP"
+*/

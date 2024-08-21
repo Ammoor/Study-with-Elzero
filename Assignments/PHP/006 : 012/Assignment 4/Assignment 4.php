7a8d20e10069d8d26e7b2c7d6d043f14c4caec53
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-08-21 09:54:38 +0300

    Create Assignment 4.php

---
+++ Assignments/PHP/006 : 012/Assignment 4/Assignment 4.php
@@ -0,0 +1,19 @@
+<?php
+// Assignment 4:
+echo nl2br("We \n Love \n Elzero \n Web \n School");
+
+/* 
+Needed Output:
+
+We
+Love
+Elzero
+Web
+School
+*/
+
+/*
+Important Note:
+    This code will be in multiple lines in the terminal not in the browser.
+    To make it in multiple lines we will use "nl2br" function.
+*/

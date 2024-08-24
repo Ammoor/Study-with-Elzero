a1c19dfec087d29f33f4fbb6b544f10647625c0f
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-08-24 22:19:16 +0300

    Update Assignment 6.php

--- Assignments/PHP/Week 2 - 006 : 012/Assignment 6/Assignment 6.php
+++ Assignments/PHP/Week 2 - 006 : 012/Assignment 6/Assignment 6.php
@@ -3,10 +3,21 @@
 
 $something = "Programming";
 
+// Edited code:
 echo <<<"code"
 Hello \PHP\
 We Love $something
 code;
+
+/* 
+Original code:
+$something = "Programming";
+
+echo <<<'code'
+Hello \PHP\
+We Love $something
+        code;
+*/
 
 // [1] Fix The Error
 // [2] Remove 2 Characters To Get The Output
@@ -15,4 +26,9 @@ code;
 Needed Output:
 
 Hello \PHP\ We Love Programming
+*/
+
+/* 
+    Important note:
+        In heredoc and nowdoc, the end title must not be precede the content of the block it self.
 */

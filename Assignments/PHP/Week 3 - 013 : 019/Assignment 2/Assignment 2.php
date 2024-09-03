de2acdddde970b26093a2e9fcfb8b29923d3950a
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-09-03 12:23:36 +0300

    Create Assignment 2.php

---
+++ Assignments/PHP/Week 3 - 013 : 019/Assignment 2/Assignment 2.php
@@ -0,0 +1,14 @@
+<?php
+// Assignment 2:
+
+$name = "elzero";
+$$name = "Web";
+
+echo $$name, "<br />";
+echo "{$$name} <br />";
+echo $elzero, "<br />";
+echo <<<"Here"
+{$$name} <br />
+Here;
+echo "Web <br />";
+echo ${$name};

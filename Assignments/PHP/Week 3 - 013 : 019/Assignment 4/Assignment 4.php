ef32b481cf5a5921f6cf0a9da9916f85e146ba0f
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-09-03 13:21:16 +0300

    Create Assignment 4.php

---
+++ Assignments/PHP/Week 3 - 013 : 019/Assignment 4/Assignment 4.php
@@ -0,0 +1,7 @@
+<?php
+// Assignment 4:
+
+echo $_SERVER["CONTEXT_DOCUMENT_ROOT"], "<br />";
+echo $_SERVER["SERVER_NAME"], "<br />";
+echo $_SERVER["SystemRoot"], "<br />";
+echo $_SERVER["OPENSSL_CONF"];

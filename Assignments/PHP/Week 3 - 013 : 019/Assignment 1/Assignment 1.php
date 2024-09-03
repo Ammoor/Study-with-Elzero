c22d4b050232fac9a3489672f09ba992c581e002
Omar Ayman Gaber <149304486+Ammoor@users.noreply.github.com>
2024-09-03 12:01:27 +0300

    Create Assignment 1.php

---
+++ Assignments/PHP/Week 3 - 013 : 019/Assignment 1/Assignment 1.php
@@ -0,0 +1,20 @@
+<!-- Assignment 1: -->
+
+<?php $var = "Elzero Courses" ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?= $var ?>">
+    <title>Welcome To <?= $var ?></title>
+</head>
+<body>
+    <h1><?= $var ?></h1>
+    <p>Here In <?= $var ?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?= $var ?> Is The What You Need.</div>
+    <footer>All Right Reserved To <?= $var ?></footer>
+</body>
+</html>
